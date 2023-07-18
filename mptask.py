c93dc648c46ff8dc515bd3c93babba8140d5b813
hldai <hldai@outlook.com>
2023-07-18 14:56:23 +0800

    update

--- mptask.py
+++ mptask.py
@@ -9,12 +9,98 @@ class MPItem:
         self.data = data
 
 
+class SingleSrcDataProcessMP:
+    def __init__(self, n_processes, n_items_per_run=50):
+        self.n_processes = n_processes
+        self.p2c_queue = mp.Queue()
+        self.c2p_queues = [mp.Queue() for _ in range(n_processes)]
+        self.n_items_per_run = n_items_per_run
+
+        self._processes = list()
+
+    def source_data_iter(self):
+        raise NotImplementedError
+
+    def process_data(self, data: MPItem) -> MPItem:
+        raise NotImplementedError
+
+    def consume_data(self, data_iter):
+        raise NotImplementedError
+
+    def _process(self, pidx):
+        processed_cnt = 0
+        while True:
+            msg, data = self.c2p_queues[pidx].get()
+            if msg == 'data':
+                result = self.process_data(data)
+                processed_cnt += 1
+                self.p2c_queue.put(('r', result))
+            elif msg == 'done':
+                self.p2c_queue.put(('done', None))
+            else:
+                break
+
+        print('process {} exit, {} items processed'.format(pidx, processed_cnt))
+
+    def run(self):
+        self._init_processes()
+
+        self.consume_data(self._processed_data_iter())
+
+        self._wait_processes()
+
+    def _src_data_distributed(self):
+        for j in range(self.n_processes):
+            self.c2p_queues[j].put(('done', None))
+
+        done_cnt = 0
+        all_results = list()
+        while done_cnt < self.n_processes:
+            msg, result = self.p2c_queue.get()
+            if msg == 'done':
+                done_cnt += 1
+            else:
+                all_results.append(result)
+
+        all_results.sort(key=lambda x: x.data_index)
+        return all_results
+
+    def _processed_data_iter(self):
+        data_cnt = 0
+        for i, src_data in enumerate(self.source_data_iter()):
+            pidx = i % self.n_processes
+            self.c2p_queues[pidx].put(('data', src_data))
+            data_cnt += 1
+            if data_cnt >= self.n_items_per_run * self.n_processes:
+                yield from self._src_data_distributed()
+                data_cnt = 0
+
+        if data_cnt > 0:
+            yield from self._src_data_distributed()
+
+        for i in range(self.n_processes):
+            self.c2p_queues[i].put(('end', None))
+
+    def _init_processes(self):
+        processes = list()
+        for i in range(self.n_processes):
+            process = mp.Process(target=self._process, args=(i,))
+            process.start()
+            processes.append(process)
+
+    def _wait_processes(self):
+        print('main waiting ...')
+        for p in self._processes:
+            p.join()
+        print('main end')
+
+
 class DataProcessMP:
-    def __init__(self, n_processes, n_lines_per_run=50):
+    def __init__(self, n_processes, n_items_per_run=50):
         self.n_processes = n_processes
         self.p2c_queue = mp.Queue()
         self.c2p_queue = mp.Queue()
-        self.n_lines_per_run = n_lines_per_run
+        self.n_items_per_run = n_items_per_run
 
         self._processes = list()
 
@@ -26,37 +112,18 @@ class DataProcessMP:
 
     def _process(self, pidx):
         cur_run_line_cnt, processed_line_cnt = 0, 0
-        is_first = True
-        for i, record in enumerate(self.produce_data(pidx)):
-            if is_first:
-                print('process {} starting from line {}'.format(pidx, i))
-                is_first = False
+        for i, item in enumerate(self.produce_data(pidx)):
+            if i == 0:
+                print('process {} start'.format(pidx))
 
             # self.p2c_queue.put(('pred', {'id': i, 'data': data}))
-            self.p2c_queue.put(('pred', record))
+            self.p2c_queue.put(('pred', item))
             processed_line_cnt += 1
             cur_run_line_cnt += 1
-            if cur_run_line_cnt == self.n_lines_per_run:
+            if cur_run_line_cnt == self.n_items_per_run:
                 self.p2c_queue.put(('done', None))
                 self.c2p_queue.get()
                 cur_run_line_cnt = 0
-
-        # f = open('d:/data/fet/ontonotes_uf/g_train.json', encoding='utf-8')
-        # for i, line in enumerate(f):
-        #     if i % self.n_processes != pidx:
-        #         continue
-        #     if is_first:
-        #         print('process {} starting from line {}'.format(pidx, i))
-        #         is_first = False
-        #
-        #     self.p2c_queue.put(('pred', {'id': i, 'data': line}))
-        #     processed_line_cnt += 1
-        #     cur_run_line_cnt += 1
-        #     if cur_run_line_cnt == self.n_lines_per_run:
-        #         self.p2c_queue.put(('done', None))
-        #         self.c2p_queue.get()
-        #         cur_run_line_cnt = 0
-        # f.close()
 
         if cur_run_line_cnt > 0:
             self.p2c_queue.put(('done', None))
@@ -67,11 +134,6 @@ class DataProcessMP:
     def run(self):
         self._init_processes()
 
-        # fout = open('d:/data/tmp/tmp_1.txt', 'w', encoding='utf-8')
-        # for i, data in enumerate(self._data_from_queue()):
-        #     for v in data:
-        #         fout.write('{}'.format(v['data']))
-        # fout.close()
         self.consume_data(self._data_from_queue())
 
         self._wait_processes()
@@ -116,9 +178,32 @@ class DataProcessMP:
         print('main end')
 
 
+class SSFileCopyMP(SingleSrcDataProcessMP):
+    def __init__(self, data_file, n_processes, n_items_per_run):
+        super().__init__(n_processes, n_items_per_run)
+        self.data_file = data_file
+
+    def source_data_iter(self):
+        f = open(self.data_file, encoding='utf-8')
+        for i, line in enumerate(f):
+            yield MPItem(i, line)
+        f.close()
+
+    def process_data(self, data: MPItem) -> MPItem:
+        return data
+
+    def consume_data(self, data_iter):
+        fout = open('d:/data/tmp/tmp_2.txt', 'w', encoding='utf-8', newline='\n')
+        for i, item in enumerate(data_iter):
+            fout.write(item.data)
+            if (i + 1) % 1000 == 0:
+                print(i + 1)
+        fout.close()
+
+
 class FileCopyMP(DataProcessMP):
-    def __init__(self, data_file, n_processes, n_lines_per_run):
-        super().__init__(n_processes, n_lines_per_run)
+    def __init__(self, data_file, n_processes, n_items_per_run):
+        super().__init__(n_processes, n_items_per_run)
         self.data_file = data_file
 
     def produce_data(self, pidx):
@@ -141,7 +226,9 @@ class FileCopyMP(DataProcessMP):
 if __name__ == '__main__':
     # mptask = DataProcessMP(4, n_lines_per_run=3)
     # mptask = FileCopyMP('d:/data/tmp/tmp_0.txt', 4, n_lines_per_run=3)
-    mptask = FileCopyMP('d:/data/fet/ontonotes_uf/ontonotes_full.json', 4, n_lines_per_run=1000)
+    # mptask = FileCopyMP('d:/data/fet/ontonotes_uf/ontonotes_full.json', 4, n_items_per_run=1000)
+    # mptask = SSFileCopyMP('d:/data/tmp/tmp_0.txt', 4, n_items_per_run=3)
+    mptask = SSFileCopyMP('d:/data/fet/ontonotes_uf/ontonotes_full.json', 4, n_items_per_run=1000)
     tbeg = time.time()
     mptask.run()
     print(time.time() - tbeg)
